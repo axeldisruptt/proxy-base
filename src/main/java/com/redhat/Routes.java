459de79ebc6a72a586d30f83d9221c6878ab4664
Axel Ramiro <axel@alchimia.mx>
2021-08-18 17:43:16 -0500

    Initial commit

--- src/main/java/com/redhat/Routes.java
+++ src/main/java/com/redhat/Routes.java
@@ -19,10 +19,10 @@ public class Routes extends RouteBuilder {
     
     rest()
       .path("/").consumes("application/json").produces("application/json")
-        .put("/customer")
+        .put("/material-list")
 //          .type(Customer.class).outType(CustomerSuccess.class)
           .to("direct:put-customer")
-        .post("/customer")
+        .post("/material-list")
 //          .type(Customer.class).outType(CustomerSuccess.class)
           .to("direct:post-customer");
     
