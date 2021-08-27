cbd07759ee5b385f503bd92bf5c076c668a6c2d6
aduranm78 <89169122+aduranm78@users.noreply.github.com>
2021-08-26 23:56:19 -0500

    Footprint-interface Tests WMS
    Interfaces PUT/POST para ambiente TEST WMS

--- src/main/java/com/redhat/Routes.java
+++ src/main/java/com/redhat/Routes.java
@@ -19,10 +19,10 @@ public class Routes extends RouteBuilder {
     
     rest()
       .path("/").consumes("application/json").produces("application/json")
-        .put("/material-list")
+        .put("/footprint")
 //          .type(Customer.class).outType(CustomerSuccess.class)
           .to("direct:put-customer")
-        .post("/material-list")
+        .post("/footprint")
 //          .type(Customer.class).outType(CustomerSuccess.class)
           .to("direct:post-customer");
     
