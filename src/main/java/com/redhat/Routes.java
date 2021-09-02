0e0366c868ca037a9e84136bcb1c9880ac5d4a92
aduranm78 <89169122+aduranm78@users.noreply.github.com>
2021-09-02 02:55:24 -0500

    Creación de la Interfaz
    Creación de la Interfaz supplier-interface WMS DISA PRD

--- src/main/java/com/redhat/Routes.java
+++ src/main/java/com/redhat/Routes.java
@@ -19,10 +19,10 @@ public class Routes extends RouteBuilder {
     
     rest()
       .path("/").consumes("application/json").produces("application/json")
-        .put("/material-list")
+        .put("/supplier-interface")
 //          .type(Customer.class).outType(CustomerSuccess.class)
           .to("direct:put-customer")
-        .post("/material-list")
+        .post("/supplier-interface")
 //          .type(Customer.class).outType(CustomerSuccess.class)
           .to("direct:post-customer");
     
