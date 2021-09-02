6ac636d11e50ef98106ceae44c1c1459ac5556cf
aduranm78 <89169122+aduranm78@users.noreply.github.com>
2021-09-02 13:40:23 -0500

    Actualización End Point
    Actualización del End Point para quitar Interface

--- src/main/java/com/redhat/Routes.java
+++ src/main/java/com/redhat/Routes.java
@@ -19,10 +19,10 @@ public class Routes extends RouteBuilder {
     
     rest()
       .path("/").consumes("application/json").produces("application/json")
-        .put("/supplier-interface")
+        .put("/supplier")
 //          .type(Customer.class).outType(CustomerSuccess.class)
           .to("direct:put-customer")
-        .post("/supplier-interface")
+        .post("/supplier")
 //          .type(Customer.class).outType(CustomerSuccess.class)
           .to("direct:post-customer");
     
