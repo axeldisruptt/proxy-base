c448fc61b901ec7a47f1f39485f025979f9ac78e
aduranm78 <89169122+aduranm78@users.noreply.github.com>
2021-09-02 13:47:18 -0500

    Actualización Endpoint
    Actualización del Endpoint, quitar la palabra interface

--- src/main/java/com/redhat/Routes.java
+++ src/main/java/com/redhat/Routes.java
@@ -19,10 +19,10 @@ public class Routes extends RouteBuilder {
     
     rest()
       .path("/").consumes("application/json").produces("application/json")
-        .put("/customer-interface")
+        .put("/customer")
 //          .type(Customer.class).outType(CustomerSuccess.class)
           .to("direct:put-customer")
-        .post("/customer-interface")
+        .post("/customer")
 //          .type(Customer.class).outType(CustomerSuccess.class)
           .to("direct:post-customer");
     
