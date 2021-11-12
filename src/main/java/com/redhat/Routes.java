dfdc981b91fd8084e68634f08e6fde2fb5d9467c
aduranm78 <89169122+aduranm78@users.noreply.github.com>
2021-11-11 18:40:14 -0600

    Actualiza valores

--- src/main/java/com/redhat/Routes.java
+++ src/main/java/com/redhat/Routes.java
@@ -19,13 +19,13 @@ public class Routes extends RouteBuilder {
     
     rest()
       .path("/").consumes("application/json").produces("application/json")
-        .put("/get_ciudades")
+        .put("/get_cities")
 //          .type(Customer.class).outType(CustomerSuccess.class)
           .to("direct:put-customer")
-        .post("/get_ciudades")
+        .post("/get_cities")
 //          .type(Customer.class).outType(CustomerSuccess.class)
           .to("direct:post-customer");
-        .get("/get_ciudades")
+        .get("/get_cities")
 //          .type(Customer.class).outType(CustomerSuccess.class)
           .to("direct:get-customer");
     
@@ -36,15 +36,44 @@ public class Routes extends RouteBuilder {
       .setHeader("HTTP_METHOD", constant("PUT"))
       .to("direct:request");
     from("direct:get-customer")
-      .setHeader("HTTP_METHOD", constant("PUT"))
+      .setHeader("HTTP_METHOD", constant("GET"))
       .to("direct:request");
 
     from("direct:request")
+      .process(new Processor() {
+        @Override
+        public void process(Exchange exchange) throws Exception {
+          String url = "https://apisgratis.com/api/codigospostales/v2/ciudades/";
+          System.out.println("URL:"+URL);
+          Message inMessage = exchange.getIn();
+          String query = inMessage.getHeader(Exchange.HTTP_QUERY, String.class);
+          System.out.println("Query:"+query);
+          if(query != null){
+              query = query + "&bridgeEndpoint=true&throwExceptionOnFailure=false";
+              System.out.println("Query is not null:"+query);
+          }else{
+              query = "bridgeEndpoint=true&throwExceptionOnFailure=false";
+              System.out.println("Query is null:"+query);
+          }
+          url = url + "?" + query; 
+          exchange.getMessage().setHeader("CamelHttpRawQuery", query);
+          System.out.println("Query:"+query);
+          exchange.getMessage().setHeader(Exchange.HTTP_URI, url);
+          System.out.println("URI:" + url);
+        }
+      })
       .setHeader("backend", simple("{{redhat.backend}}"))
       .setHeader("", simple("*"))
       .to("log:DEBUG?showBody=true&showHeaders=true")
+      .to("https://netsuite")
+      .streamCaching()
+      .log(LoggingLevel.INFO, "${in.headers.CamelFileName}")
+      .to("log:DEBUG?showBody=true&showHeaders=true")
+      /*
+      .to("log:DEBUG?showBody=true&showHeaders=true")
       .toD("http://${header.backend}?bridgeEndpoint=true&throwExceptionOnFailure=false")
       .to("log:DEBUG?showBody=true&showHeaders=true");
+      */
       
 //      .choice()
 //        .when(simple("${header.CamelHttpResponseCode} != 201 && ${header.CamelHttpResponseCode} != 202"))
